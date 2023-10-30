1d9b8bba7cd775024ffee053ed4923e18adec468
Grzegorz Drozd <grzegorz.drozd@gmail.com>
2023-10-30 20:00:46 +0100

    Fix unit tests.

--- tests/StringTest.php
+++ tests/StringTest.php
@@ -16,19 +16,19 @@ class StringTest extends TestCase
         $parser = new SitemapParser('SitemapParser', ['strict' => false]);
         $this->assertInstanceOf('vipnytt\SitemapParser', $parser);
         $parser->parse($url);
-        $this->assertInternalType('array', $parser->getSitemaps());
-        $this->assertInternalType('array', $parser->getURLs());
+        $this->assertIsArray($parser->getSitemaps());
+        $this->assertIsArray($parser->getURLs());
         $this->assertTrue(count($parser->getSitemaps()) > 1);
         $this->assertTrue(count($parser->getURLs()) >= 1000);
         foreach ($parser->getSitemaps() as $url => $tags) {
-            $this->assertInternalType('string', $url);
-            $this->assertInternalType('array', $tags);
+            $this->assertIsString($url);
+            $this->assertIsArray($tags);
             $this->assertTrue($url === $tags['loc']);
             $this->assertNotFalse(filter_var($url, FILTER_VALIDATE_URL));
         }
         foreach ($parser->getURLs() as $url => $tags) {
-            $this->assertInternalType('string', $url);
-            $this->assertInternalType('array', $tags);
+            $this->assertIsString($url);
+            $this->assertIsArray($tags);
             $this->assertTrue($url === $tags['loc']);
             $this->assertNotFalse(filter_var($url, FILTER_VALIDATE_URL));
         }
