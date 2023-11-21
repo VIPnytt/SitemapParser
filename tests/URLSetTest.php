ebd82ce968b1316147bf3113d192f7abf920618d
Grzegorz Drozd <grzegorz.drozd@gmail.com>
2023-11-21 14:06:33 +0100

    Improve handling of additional namespaced objects
    Updated the `objectToArray` method to handle SimpleXMLElement objects in a fine-grained way. More specifically, the function now iterates through and properly unpacks the attributes of such objects - like alternative links or languages.

--- tests/URLSetTest.php
+++ tests/URLSetTest.php
@@ -16,10 +16,10 @@ class URLSetTest extends TestCase
     public function testURLSet($url, $body, $result)
     {
         $parser = new SitemapParser('SitemapParser');
-        $this->assertInstanceOf('vipnytt\SitemapParser', $parser);
+        self::assertInstanceOf('vipnytt\SitemapParser', $parser);
         $parser->parse($url, $body);
-        $this->assertEquals([], $parser->getSitemaps());
-        $this->assertEquals($result, $parser->getURLs());
+        self::assertEquals([], $parser->getSitemaps());
+        self::assertEquals($result, $parser->getURLs());
     }
 
     /**
@@ -34,12 +34,14 @@ class URLSetTest extends TestCase
 
                 <<<XMLSITEMAP
 <?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">
    <url>
       <loc>http://www.example.com/</loc>
       <lastmod>2005-01-01</lastmod>
       <changefreq>monthly</changefreq>
       <priority>0.8</priority>
+      <xhtml:link rel="alternate" hreflang="en-US" href="http://www.example.com/?land=en&amp;country=US" />
+      <xhtml:link rel="alternate" hreflang="en-GB" href="http://www.example.com/?land=en&amp;country=GB" />
    </url>
    <url>
       <loc>http://www.example.com/catalog?item=12&amp;desc=vacation_hawaii</loc>
@@ -62,41 +64,66 @@ class URLSetTest extends TestCase
 </urlset>
 XMLSITEMAP
                 ,
-                $result = [
+                [
                     'http://www.example.com/' => [
                         'loc' => 'http://www.example.com/',
                         'lastmod' => '2005-01-01',
                         'changefreq' => 'monthly',
                         'priority' => '0.8',
-                        'namespaces'=> [],
+                        'namespaces'=> [
+                            'xhtml' => [
+                                [
+                                    'link' => [
+                                        'rel' => 'alternate',
+                                        'hreflang' => 'en-US',
+                                        'href' => 'http://www.example.com/?land=en&country=US'
+                                    ]
+                                ],
+                                [
+                                    'link' => [
+                                        'rel' => 'alternate',
+                                        'hreflang' => 'en-GB',
+                                        'href' => 'http://www.example.com/?land=en&country=GB'
+                                    ]
+                                ],
+                            ],
+                        ],
                     ],
                     'http://www.example.com/catalog?item=12&desc=vacation_hawaii' => [
                         'loc' => 'http://www.example.com/catalog?item=12&desc=vacation_hawaii',
                         'changefreq' => 'weekly',
                         'lastmod' => null,
                         'priority' => null,
-                        'namespaces'=> [],
+                        'namespaces'=> [
+                            'xhtml' => [],
+                        ],
                     ],
                     'http://www.example.com/catalog?item=73&desc=vacation_new_zealand' => [
                         'loc' => 'http://www.example.com/catalog?item=73&desc=vacation_new_zealand',
                         'lastmod' => '2004-12-23',
                         'changefreq' => 'weekly',
                         'priority' => null,
-                        'namespaces'=> [],
+                        'namespaces'=> [
+                            'xhtml' => [],
+                        ],
                     ],
                     'http://www.example.com/catalog?item=74&desc=vacation_newfoundland' => [
                         'loc' => 'http://www.example.com/catalog?item=74&desc=vacation_newfoundland',
                         'lastmod' => '2004-12-23T18:00:15+00:00',
                         'priority' => '0.3',
                         'changefreq' => null,
-                        'namespaces'=> [],
+                        'namespaces'=> [
+                            'xhtml' => [],
+                        ],
                     ],
                     'http://www.example.com/catalog?item=83&desc=vacation_usa' => [
                         'loc' => 'http://www.example.com/catalog?item=83&desc=vacation_usa',
                         'lastmod' => '2004-11-23',
                         'changefreq' => null,
                         'priority' => null,
-                        'namespaces'=> [],
+                        'namespaces'=> [
+                            'xhtml' => [],
+                        ],
                     ],
                 ]
             ]
